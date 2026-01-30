6a3eacdfefa91ba9f9ce50f6a592130db6106312
elvis <omondielvis.owuor@gmail.com>
2026-01-30 17:44:28 +0300

    maven-branch-naming

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
-      // "no-console": "warn",
+      "no-console": "warn",
       "no-unused-vars": "warn",
     },
   },
