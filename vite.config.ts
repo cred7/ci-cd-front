38c0474f97a6825a4643927caa008161611aae83
elvis <omondielvis.owuor@gmail.com>
2026-02-01 21:45:17 +0300

    merged-buis

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ci-cd-front/",
+  // base: "/ci-cd-front/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
